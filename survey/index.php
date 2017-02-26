14f22631560e717bc9d08490f6c9392ad60ea0a8
Luigi Battaglioli <lbattaglioli2000@gmail.com>
2017-02-26 22:34:58 +0000

    Setup the survey page.

--- survey/index.php
+++ survey/index.php
@@ -7,7 +7,7 @@
   <html>
 
   <head>
-    <title>Energy Performance</title>
+    <title>Survey</title>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -22,29 +22,19 @@
     <?php include('../includes/nav.php'); ?>
     <div class="jumbotron">
       <div class="container">
-        <h1>Energy Performance</h1>
+        <h1>10 Second Survey</h1>
       </div>
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-md-9">
-          <p>Some builders are attempting to achieve the new standards using labor-intense methods such as caulking interior framing gaps, and/or taping and rolling exterior sheathing seams with specialty tapes. Taping of sheathing seams and water resistive barriers certainly helps to mitigate air intrusion, however in doing so air intrusion then shifts to air entrance gaps that remain unsealed; these open lineal gaps are filled with a continuous gasket sandwiched between framing components when <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> is used, effectively reducing air flow. A much more durable solution than caulk/tapes that relies on surface bonding for air intrusion.</p>
-          <p><i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> has been shown (by a New York State Energy Agency) to effectively reduce air intrusion by over 80% when tested at 50 pascals in a typical wood-frame wall. In addition to its air isolation properties, <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> costs far less in materials and labor than other methods such as caulking and taping and blown-in types of insulation offering builders increased insulation options while complying with code.</p>
-          <p>The on-site manufacturing concept of <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> provides an air isolation gasket that serves as a framing guide to optimize labor. This simple innovation speeds up completion time, reduces common framing errors, and allows the utilization of less-skilled workers with only minimal system training needed, an additional savings benefit in today's tight labor market.</p>
-          <p><i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> has been tested in the laboratory and in the field with excellent results. Recent side by side testing of two identical houses built by Habitat for Humanity with and without <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> showed that the home without AccuFrame did not reach the current required standards for air infiltration while the home with AccuFrame easily qualified. The current requirement in the test area is less than 3 Air Changes per Hour (ACH) to qualify. The house with <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> measured 2.63 ACH whereas the house without  reached 3.81 ACH a failing test result. Additionally, the house without <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> had been sealed from the interior using silicone caulking along framing and sheathing seams, a practice not usually employed. Caulking decreases air flow, and the result obtained for the house without <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> would have been even further from qualifying if built with today's common practices.</p>
-          <p>Not only did <i>The AccuFrame<sup><small>&reg;</small></sup> Energy Seal</i> house meet the current requirement with fiberglass batt insulation, but it also reached the 2018 test level which will increase air sealing requirements in the near future.</p>
-        </div>
-        <div class="col-md-3">
-          <div class="panel panel-primary">
-            <div class="panel-heading">Importnant Links</div>
-            <div class="panel-body">
-              <ul>
-                <li><a href="#">ASTMe 283 Test Data</a></li>
-                <li><a href="#">Builders Information</a></li>
-                <li><a href="#">Testing and Specifications of <i>Habitat For Humanity</i> Results</a></li>
-              </ul>
-            </div>
+        <div class="col-md-12">
+          <div class="alert alert-info" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>Hey!</strong> If you don't mind, please fill out this survey to help us create an even better product!
           </div>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdhhtCRFISeE2MpX3jreyC73G3PbmAo_vJBuJCDGx6U8hQq7g/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         </div>
       </div>
         <footer>
