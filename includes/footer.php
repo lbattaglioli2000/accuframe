34e26aa89ebe6f90e76195d2e4f47d21b8fa1c0d
Luigi Battaglioli <Luigi Battaglioli>
2017-03-05 00:48:27 -0400

    added live chat

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,26 @@
-<script type='text/javascript'>var fc_JS=document.createElement('script');fc_JS.type='text/javascript';fc_JS.src='https://assets1.freshchat.io/production/assets/widget.js?t='+Date.now();(document.body?document.body:document.getElementsByTagName('head')[0]).appendChild(fc_JS); window._fcWidgetCode='2E7EJ8QOZD';window._fcURL='https://accuframe.freshchat.io';</script>
+<!-- Start of Async Drift Code -->
+<script>
+!function() {
+  var t;
+  if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
+  t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+  t.factory = function(e) {
+    return function() {
+      var n;
+      return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
+    };
+  }, t.methods.forEach(function(e) {
+    t[e] = t.factory(e);
+  }), t.load = function(t) {
+    var e, n, o, i;
+    e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
+    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
+    n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
+  });
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('66dfgkfk8s8x');
+</script>
+<!-- End of Async Drift Code -->
 <p class="pull-right"><a href="#">Back to top</a></p>
 <p><?php echo date("Y"); ?> AccuFrame<sup><small>&reg;</small></sup> Energy Seal &middot; Site Designed and Built by <a href="http://www.arrested-yeti.hol.es">Arrested Yeti</a></p>
