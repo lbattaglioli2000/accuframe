7b7c7e8ea739bf631dab5e026868651152b48fd5
Luigi Battaglioli <lbattaglioli2000@gmail.com>
2017-04-08 00:32:38 +0000

    Added privacy and other minor changes such as mailchimp signup

--- index.php
+++ index.php
@@ -189,7 +189,29 @@ As of early 2017, nine states (with others to follow soon) require compliance wi
                     </div>
                 </div>
                 <hr class="featurette-divider">
+                <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<h2 style="text-align: center;">Sign up for our email list!</h2>
+<div id="mc_embed_signup">
+<form action="//accuframe.us15.list-manage.com/subscribe/post?u=88567388e18258d7134dc05f0&amp;id=0bf02630fb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="form-control" id="mce-EMAIL" placeholder="Email address" required><br>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_88567388e18258d7134dc05f0_0bf02630fb" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary"></div>
+    </div>
+</form>
+</div>
 
+<!--End mc_embed_signup-->
+<hr class="featurette-divider">
+<!--End mc_embed_signup-->
                 <footer>
                     <?php include("includes/footer.php"); ?>
                 </footer>
